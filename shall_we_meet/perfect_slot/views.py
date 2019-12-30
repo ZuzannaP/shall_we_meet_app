eaa7e0b5fe3f418e64baa56c58bacfd9f551c80b
zuzanna <zpietruszynska@gmail.com>
2019-12-30 13:01:24 +0100

    custom user created, login and logout created

--- shall_we_meet/perfect_slot/views.py
+++ shall_we_meet/perfect_slot/views.py
@@ -1,8 +1,48 @@
+from django.contrib.auth import authenticate, logout, login
 from django.shortcuts import render
+from django.urls import reverse_lazy
+from django.views import View
+from django.views.generic.edit import CreateView, FormView
+from .forms import CustomUserCreationForm, LoginForm
 
 
 def homepage(request):
     return render(request, "homepage.html")
 
 
+class LoginView(FormView):
+    form_class = LoginForm
+    success_url = "/"
+    template_name = "login.html"
 
+    def form_valid(self, form: LoginForm):
+        username = form.cleaned_data["username"]
+        password = form.cleaned_data["password"]
+        user = authenticate(self.request, username=username, password=password)
+
+        if user is None:
+            form.add_error(None, "Incorrect login or password")
+            return super().form_invalid(form)
+
+        login(self.request, user)
+        return super().form_valid(form)
+
+
+class LogoutView(View):
+    def get(self, request):
+        return render(request, "logout.html")
+
+    def post(self, request):
+        ctx = {}
+        logout(request)
+        if request.user.is_authenticated:
+            ctx["my_verdict"] = "Ups. Something went wrong."
+        else:
+            ctx["my_verdict"] = "You have been logged out"
+        return render(request, "logout.html", ctx)
+
+
+# class SignUpView(CreateView):
+#     form_class = CustomUserCreationForm
+#     success_url = reverse_lazy('login')
+#     template_name = 'signup.html'
