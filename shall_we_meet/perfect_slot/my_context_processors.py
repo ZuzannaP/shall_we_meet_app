49ff3917df443041530ef9d83bd799525c02341d
zuzanna <zpietruszynska@gmail.com>
2020-01-20 21:10:42 +0100

    added context_processor that enables PENDING notifications in real time

--- shall_we_meet/perfect_slot/my_context_processors.py
+++ shall_we_meet/perfect_slot/my_context_processors.py
@@ -6,11 +6,20 @@ from .models import Event, ParticipantSlotVote
 def navbar_input(request):
     if request.user.is_authenticated:
         owner_pending_actions = Event.objects.filter(owner=request.user).filter(is_in_progress=True)
-        guest_pending_actions = Event.objects.filter(Q(is_in_progress=True) & Q(participants=request.user)).distinct()
+        events = Event.objects.filter(participants=request.user).filter(is_in_progress=True)
+        guest_pending_actions = 0
+        for event in events:
+            timeslots = event.event_datetimeslot.all()
+            for slot in timeslots:
+                my_vote = ParticipantSlotVote.objects.filter(participant=request.user).filter(slot=slot)[0]
+                if my_vote.vote == -2:
+                    guest_pending_actions += 1
+                    break
         ctx = {"owner_pending_actions": owner_pending_actions, "guest_pending_actions": guest_pending_actions}
         return ctx
     else:
         ctx = {"owner_pending_actions": "n/a", "guest_pending_actions": "n/a"}
         return ctx
+
 
 
