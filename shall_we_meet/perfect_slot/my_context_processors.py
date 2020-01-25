df584b4a08cf7c343742f449a61c862676e5cd23
Zuzanna <zpietruszynska@gmail.com>
2020-01-25 16:44:45 +0100

    cośtam

--- shall_we_meet/perfect_slot/my_context_processors.py
+++ shall_we_meet/perfect_slot/my_context_processors.py
@@ -6,6 +6,7 @@ from .models import Event, ParticipantSlotVote
 def navbar_input(request):
     if request.user.is_authenticated:
         owner_pending_actions = Event.objects.filter(owner=request.user).filter(is_in_progress=True)
+        owner_upcoming_events = Event.objects.filter(owner=request.user).filter(is_upcoming=True)
         events = Event.objects.filter(participants=request.user).filter(is_in_progress=True)
         guest_pending_actions = 0
         for event in events:
@@ -15,10 +16,12 @@ def navbar_input(request):
                 if my_vote.vote == -2:
                     guest_pending_actions += 1
                     break
-        ctx = {"owner_pending_actions": owner_pending_actions, "guest_pending_actions": guest_pending_actions}
+        guest_upcoming_events = Event.objects.filter(participants=request.user).filter(is_upcoming=True)
+        ctx = {"owner_pending_actions": owner_pending_actions, "owner_upcoming_events": owner_upcoming_events, \
+               "guest_pending_actions": guest_pending_actions, "guest_upcoming_events": guest_upcoming_events}
         return ctx
     else:
-        ctx = {"owner_pending_actions": "n/a", "guest_pending_actions": "n/a"}
+        ctx = {"owner_pending_actions": "n/a", "owner_upcoming_events":"n/a", "guest_pending_actions": "n/a", "guest_upcoming_events":"n/a"}
         return ctx
 
 
