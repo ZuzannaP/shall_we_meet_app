1b6f9c2c81b2721b5c78be45c79b38735868c46b
zuzanna <zpietruszynska@gmail.com>
2020-02-13 22:15:58 +0100

    added forms and models tests

--- shall_we_meet/perfect_slot/tests.py
+++ shall_we_meet/perfect_slot/tests.py
@@ -2,7 +2,37 @@ from django.test import TestCase
 from django.test import Client
 from django.urls import reverse
 
-from perfect_slot.models import CustomUser, Event
+from perfect_slot.forms import CustomUserChangeForm, ChooseMeetingLocationForm, EditEventForm, \
+                                EditMeetingLocationForm
+from perfect_slot.models import CustomUser, Event, DateTimeSlot, ParticipantSlotVote
+
+
+class ModelTestClass(TestCase):
+    @classmethod
+    def setUpTestData(cls):
+        cls.c = CustomUser.objects.create_user(first_name="Pan", last_name="Tester", username="pan_tester",
+                                               password="pantesterpantester", email="pan@wp.pl",
+                                               geographical_coordinates="Point(12 12)")
+        cls.e = Event.objects.create(title="My event", description="Great event", owner=cls.c)
+        cls.d = DateTimeSlot.objects.create(date_time_from="2021-03-18 17:30:00+01",
+                                            date_time_to="2021-03-18 20:00:00+01", event=cls.e)
+        cls.p = ParticipantSlotVote.objects.create(participant=cls.c, slot=cls.d, vote=1)
+
+    def test_user(self):
+        self.assertTrue(isinstance(self.c, CustomUser))
+        self.assertEqual(self.c.__str__(), self.c.username)
+
+    def test_event(self):
+        self.assertTrue(isinstance(self.e, Event))
+        self.assertEqual(self.e.__str__(), self.e.title)
+
+    def test_datetime_slot(self):
+        self.assertTrue(isinstance(self.d, DateTimeSlot))
+        self.assertEqual(self.d.__str__(), f"{self.d.date_time_from} - {self.d.date_time_to}")
+
+    def test_participant_slot_vote(self):
+        self.assertTrue(isinstance(self.p, ParticipantSlotVote))
+        self.assertEqual(self.p.__str__(), f"{self.p.participant}-{self.p.slot}: {self.p.vote}")
 
 
 class ViewsTestClass(TestCase):
@@ -31,22 +61,83 @@ class ViewsTestClass(TestCase):
 
 
 class FormsTestClass(TestCase):
-    # def setUp(self):
-    #     print("setUp: Run once for every test method to setup clean data.")
-    #     pass
+    @classmethod
+    def setUpTestData(cls):
+        cls.c = CustomUser.objects.create_user(first_name="Pan", last_name="Tester", username="pan_tester",
+                                               password="pantesterpantester", email="pan@wp.pl",
+                                               geographical_coordinates="Point(12 12)")
+        cls.e = Event.objects.create(title="My event", description="Great event", owner=cls.c)
+        cls.d = DateTimeSlot.objects.create(date_time_from="2021-03-18 17:30:00+01",
+                                            date_time_to="2021-03-18 20:00:00+01", event=cls.e)
+        cls.p = ParticipantSlotVote.objects.create(participant=cls.c, slot=cls.d, vote=1)
 
-    def test_mock(self):
-        u = 4
-        self.assertEqual(u, 1)
-        print("do stuff4")
+    def test_valid_custom_user_change_form(self):
+        self.c = CustomUser.objects.create_user(first_name="Pan1", last_name="Tester1", username="pan_tester1",
+                                                password="pantester1pantester1", email="pan1@wp.pl",
+                                                geographical_coordinates="Point(12 12)")
+        data = {'first_name': self.c.first_name, 'last_name': self.c.last_name, 'email': self.c.email,
+                'geographical_coordinates': self.c.geographical_coordinates}
+        form = CustomUserChangeForm(data=data)
+        self.assertTrue(form.is_valid())
 
+    def test_invalid_custom_user_change_form(self):
+        self.c = CustomUser.objects.create_user(first_name="Pan2", last_name="Tester2", username="pan_tester2",
+                                                password="pantester2pantester2", email="",
+                                                geographical_coordinates="Point(12 12)")
+        data = {'first_name': self.c.first_name, 'last_name': self.c.last_name, 'email': self.c.email,
+                'geographical_coordinates': self.c.geographical_coordinates}
+        form = CustomUserChangeForm(data=data)
+        self.assertFalse(form.is_valid())
 
-class ModelTestClass(TestCase):
-    def setUp(self):
-        self.test_event = Event.objects.create(title="My event", description="Great event")
-        print("do stuff5")
+    def test_valid_choose_meeting_location_form(self):
+        self.e.meeting_address = "Nice Street"
+        self.e.meeting_geographical_coordinates = "Point(13 13)"
+        self.e.location_comments = "The key is under doormat"
+        data = {'meeting_address': self.e.meeting_address,
+                "meeting_geographical_coordinates": self.e.meeting_geographical_coordinates,
+                "location_comments": self.e.location_comments}
+        form = ChooseMeetingLocationForm(data=data)
+        self.assertTrue(form.is_valid())
 
-    def test_my(self):
-        e = Event.objects.get(pk=1)
-        self.assertEqual(e.title, "My event")
-        print("do stuff6")
+    def test_invalid_choose_meeting_location_form(self):
+        """omitting "meeting_geographical_coordinates" on purpose"""
+        self.e.meeting_address = "Big Street"
+        self.e.meeting_geographical_coordinates = "Point(13 13)"
+        self.e.location_comments = "The key is vase"
+        data = {'meeting_address': self.e.meeting_address,
+                "meeting_geographical_coordinates": "",
+                "location_comments": self.e.location_comments}
+        form = ChooseMeetingLocationForm(data=data)
+        self.assertFalse(form.is_valid())
+
+    def test_valid_edit_event_form(self):
+        self.test_participant = CustomUser.objects.create_user(first_name="PanPat", last_name="Participant",
+                                                               username="pan_participant",
+                                                               password="pan_participantpan_participant",
+                                                               email="participant@wp.pl",
+                                                               geographical_coordinates="Point(14 14)")
+        self.e.participants.add(self.test_participant)
+        form = EditEventForm(excluding_owner=self.c)
+        displayed_participants = form.fields['participants'].queryset.all()
+        self.assertTrue(self.c not in displayed_participants)
+
+    def test_valid_edit_meeting_location_form(self):
+        self.e.meeting_address = "Big Street"
+        self.e.meeting_geographical_coordinates = "Point(4 4)"
+        self.e.location_comments = "There are no doors. Use window"
+        data = {'meeting_address': self.e.meeting_address,
+                "meeting_geographical_coordinates": self.e.meeting_geographical_coordinates,
+                "location_comments": self.e.location_comments}
+        form = EditMeetingLocationForm(data=data)
+        self.assertTrue(form.is_valid())
+
+    def test_invalid_edit_meeting_location_form(self):
+        """omitting "meeting_address" on purpose"""
+        self.e.meeting_address = "Big Street"
+        self.e.meeting_geographical_coordinates = "Point(4 4)"
+        self.e.location_comments = "There are no doors. Use window"
+        data = {'meeting_address': "",
+                "meeting_geographical_coordinates": self.e.meeting_geographical_coordinates,
+                "location_comments": self.e.location_comments}
+        form = EditMeetingLocationForm(data=data)
+        self.assertFalse(form.is_valid())
