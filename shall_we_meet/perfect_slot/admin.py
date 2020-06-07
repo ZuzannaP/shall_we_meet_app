2b9a368a735e807efc2f19c36a8fecd8d49b81a4
zuzanna <zpietruszynska@gmail.com>
2020-06-07 16:32:09 +0200

    modified has_add_permission in django admin CustomUser

--- shall_we_meet/perfect_slot/admin.py
+++ shall_we_meet/perfect_slot/admin.py
@@ -1,18 +1,26 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
 
-from .forms import CustomUserCreationForm
+from .forms import CustomUserCreationForm, CustomUserChangeForm
 from .models import CustomUser, Event, DateTimeSlot, ParticipantSlotVote
 
 
 class CustomUserAdmin(UserAdmin):
     add_form = CustomUserCreationForm
+    form = CustomUserChangeForm
     model = CustomUser
-    list_display = ['username', 'is_active', ]
+    list_display = ['username','first_name', 'last_name', 'is_active']
 
     fieldsets = UserAdmin.fieldsets + (
-        (None, {'fields': (['geographical_coordinates'])}),
+        (None, {'fields': ([ 'geographical_coordinates'])
+    }),
     )
+
+    # unables 'add user' functionality in DjangoAdmin (as it can't be done correctly
+    # with current geographical_coordinates logic)
+    def has_add_permission(self, request):
+        return False
+
 
 
 admin.site.register(CustomUser, CustomUserAdmin),
